c54dae5a1a6131298f3172b91381e135f1dcfc52
David Geiter <dag@panter.ch>
2019-07-24 23:39:13 +0200

    fix resource path

--- halbmarathon-schatzsuechi-backend/src/main/java/shop/halbmarathon/halbmarathonschatzsuechi/currency/CurrencyService.java
+++ halbmarathon-schatzsuechi-backend/src/main/java/shop/halbmarathon/halbmarathonschatzsuechi/currency/CurrencyService.java
@@ -49,7 +49,7 @@ public class CurrencyService {
 
 	private Optional<QrCodeDto> getQrCode(final String qrId) {
 		List<List<String>> records = new ArrayList<>();
-		try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/qrCodes.csv"))) {
+		try (BufferedReader br = new BufferedReader(new FileReader("halbmarathon-schatzsuechi-backend/src/main/resources/qrCodes.csv"))) {
 			String line;
 			while ((line = br.readLine()) != null) {
 				String[] values = line.split(",");
