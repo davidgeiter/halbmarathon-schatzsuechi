0089676f949e8e66936496a6677ee98010ca4408
AdrianKrebs <kra@panter.ch>
2019-07-24 20:53:32 +0200

    fix

--- halbmarathon-schatzsuechi-backend/src/frontend/src/components/Layout.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components"
-
-export default styled.div`
-  width: 80vw;
-  margin: 0 auto;
-`
--- halbmarathon-schatzsuechi-backend/src/frontend/src/stories/decorators.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { GlobalStyles } from "../lib/globalStyles"
-
-export const withGlobalStyles = storyFn => (
-  <React.Fragment>
-    <GlobalStyles />
-    {storyFn()}
-  </React.Fragment>
-)
